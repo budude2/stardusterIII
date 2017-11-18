ef82dd26b2630d5766e05cf128f7521dea73ea3c
Jacob Boline <mail@jboline.us>
2017-11-18 08:28:03 -0600

    Add GPS parsing, and clean breaking

--- aprs/test_aprs.py
+++ aprs/test_aprs.py
@@ -2,19 +2,88 @@
 # -*- coding: utf-8 -*-
 
 import serial
-from aprslib.packets import PositionReport
+import pynmea2
 import utils
+from threading import Thread
+import time
+import sys
+import signal
 
-#ser = serial.Serial('/dev/ttyUSB0', 119200)
+# aprstnc = serial.Serial('/dev/ttyUSB0', 119200)
 
-mypkt = utils.position_pkt()
+# Setup GPS Serial port
+gps = serial.Serial()
+gps.baudrate = 9600
+gps.port = '/dev/tty.usbserial-A5058GNQ'
 
-mypkt.latitude  = 33.103172
-mypkt.longitude = -96.670547
-mypkt.icon      = "O"
-mypkt.heading   = 271
-mypkt.speed     = 12
-mypkt.altitude  = 600
+# Global Variables
+exitApp = False
+lat = 0
+lon = 0
+alt = 0
 
-print(str(mypkt))
-print(mypkt.serialize())
+def sigint_handler(signum, frame):
+    global exitApp
+
+    print('\nClosing...\n')
+    exitApp = True
+
+signal.signal(signal.SIGINT, sigint_handler)
+
+try:
+    gps.open()
+except serial.SerialException:
+    print("Could not open serial port.")
+    print("Is it configured correctly?")
+    sys.exit(0)
+
+def get_gps():
+    global lat
+    global lon
+    global alt
+
+    while (exitApp == False):
+        data = gps.readline().decode('ascii')
+        try:
+            msg = pynmea2.parse(data)
+
+            if isinstance(msg, pynmea2.types.talker.GGA):
+                lat = msg.latitude
+                lon = msg.longitude
+
+                print("Latitude:", lat)
+                print("Longitude:", lon)
+                print("Numb Sats:", msg.num_sats)
+
+                if(msg.altitude_units == 'M'):
+                    alt = msg.altitude * (1/0.3048)
+
+                print("Altitude:", str(round(alt, 2)), "ft\n")
+        except:
+            pass
+
+    gps.close()
+
+def aprs():
+    mypkt = utils.position_pkt()
+
+    while (exitApp == False):
+        print('loop')
+        print(exitApp)
+        mypkt.latitude  = lat
+        mypkt.longitude = lon
+        mypkt.icon      = "O"
+        mypkt.heading   = 271
+        mypkt.speed     = 12
+        mypkt.altitude  = alt
+
+        print(str(mypkt))
+        print(mypkt.serialize())
+
+        time.sleep(5)
+
+gps_thread = Thread(target = get_gps)
+gps_thread.start()
+
+aprs_thread = Thread(target = aprs)
+aprs_thread.start()
