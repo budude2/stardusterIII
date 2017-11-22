670a91477926ca4ebb8717912995e9fb203fb6ef
Jacob Boline <mail@jboline.us>
2017-11-21 22:44:05 -0600

    Add temperature probe

--- data/data.py
+++ data/data.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import max31865
 import HSC
 import HTU21D
 
 pres = HSC.HSC(1)
 hum = HTU21D.HTU21D(1)
+temp = max31865.max31865(8,9,10,11)
 
 print("Pressure (mb): " + str(pres.read_pressure_mb()))
 print("Humidity (rh): " + str(round(hum.read_humidity(), 2)))
 print("Temp (c): " + str(round(hum.read_temperature(), 2)))
+print("Thermo temp (c): " + str(round(temp.readTemp(), 2)))
