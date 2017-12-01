d190d53e6b4376edbdca386871f8753a557ed455
Jacob Boline <mail@jboline.us>
2017-11-30 19:09:04 -0600

    replace smbus

--- data/HTU21D.py
+++ data/HTU21D.py
@@ -1,4 +1,4 @@
-from smbus import SMBus
+import pigpio
 
 I2C_ADDR           = 0x40
 CMD_TRIG_TEMP_HM   = 0xE3
@@ -11,22 +11,24 @@ CMD_RESET          = 0xFE
     
 class HTU21D:
     def __init__(self, busno):
-        self.bus = SMBus(busno)
+        self.bus = pigpio.pi()
+        self.handle = self.bus.i2c_open(busno, I2C_ADDR, 0)
 
     def read_temperature(self):
         self.reset()
-        msb, lsb, crc = self.bus.read_i2c_block_data(I2C_ADDR, CMD_TRIG_TEMP_HM, 3)
-        return -46.85 + 175.72 * (msb * 256 + lsb) / 65536
+        (stat, data) = self.bus.i2c_read_i2c_block_data(self.handle, CMD_TRIG_TEMP_HM, 3)
+        self.bus.i2c_close(self.handle)
+        return -46.85 + 175.72 * (data[1] * 256 + data[0]) / 65536
      
     def read_humidity(self):
         self.reset()
-        msb, lsb, crc = self.bus.read_i2c_block_data(I2C_ADDR, CMD_TRIG_HUMID_HM, 3)
-        return -6 + 125 * (msb * 256 + lsb) / 65536.0
+        (stat, data) = self.bus.i2c_read_i2c_block_data(self.handle, CMD_TRIG_HUMID_HM, 3)
+        return -6 + 125 * (data[0] * 256 + data[1]) / 65536.0
 
     def reset(self):
-        self.bus.write_byte(I2C_ADDR, CMD_RESET)
+        self.bus.i2c_write_byte(self.handle, CMD_RESET)
 
 if __name__ == '__main__':
    htu = HTU21D(1)
-   print(htu.read_temperature())
+   #print(htu.read_temperature())
    print(htu.read_humidity())
